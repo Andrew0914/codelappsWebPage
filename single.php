15ba19d2e37104fcf18a2ad8e976f6a0684b4a9b
AndrewAlan <andrewalangm@gmail.com>
2019-07-28 01:54:52 -0500

    Present categories

--- single.php
+++ single.php
@@ -1,52 +1,72 @@
 <?php
     get_header();
 ?>
+<div class="row">
+    <div class="col s12">
+<?php while(have_posts()): the_post(); ?>  
 <div class="card mt1 z-depth-2">
     <div class="card-content">
-        <?php while(have_posts()): the_post(); ?>  
+        <!-- TITLE -->
+        <span class="card-title">
+            <b><?php the_title(); ?></b>
 
-            <span class="card-title">
-                <b><?php the_title(); ?></b>
+            <?php if( get_field('tipo_desarrollo') ){ ?>
+            <div class="chip big-text">
+                <small>
+                    <?php the_field('tipo_desarrollo'); ?>
+                </small>
+            </div>
+            <?php } ?>
 
-                <?php if( get_field('tipo_desarrollo') ){ ?>
-                <div class="chip big-text">
-					<small>
-						<?php the_field('tipo_desarrollo'); ?>
-					</small>
-                </div>
-                <?php } ?>
-
-            </span>
-
-            <?php the_content(); ?>
-
-        <?php endwhile; ?>
+        </span>
+        <!-- CONTENT -->
+        <?php the_content(); ?>
+        <!-- ACTIONS -->
+        <div class="card-action">
+            <?php if (get_field('enlace')) { ?>
+                <a href="<?php the_field('enlace'); ?>" class="tooltipped" data-position="bottom" data-tooltip="demo">
+                    <b>Ver en vivo &nbsp;<i class="fas fa-external-link-alt"></i></b>
+                </a>
+            <?php } ?>
+            <?php	if (get_field('url_descarga')) { ?>
+                <a href="<?php the_field('url_descarga'); ?>" class="tooltipped" data-position="top" data-tooltip="descargar">
+                <b>Descargar&nbsp;<i class="fas fa-download"></i></b>
+                </a>
+            <?php } ?>
+            <?php if (get_field('url_demo')) { ?>
+                <a href="<?php the_field('url_demo'); ?>" class="tooltipped" data-position="bottom" data-tooltip="demo">
+                    <b>Demostración &nbsp;<i class="fas fa-external-link-alt"></i></b>
+                </a>
+            <?php } ?>
+            <?php if (get_field('url_video')) { ?>
+                <a href="<?php the_field('url_video'); ?>" class="tooltipped" data-position="bottom" data-tooltip="demo">
+                    <b>Ver en Youtube &nbsp;<i class="fab fa-youtube"></i></b>
+                </a>
+            <?php } ?>
+        </div>
     </div>
-
-    <div class="card-action">
-        <?php if (get_field('enlace')) { ?>
-            <a href="<?php the_field('enlace'); ?>" class="tooltipped" data-position="bottom" data-tooltip="demo">
-                <b>Ver en vivo &nbsp;<i class="fas fa-external-link-alt"></i></b>
-            </a>
-        <?php } ?>
-        <?php	if (get_field('url_descarga')) { ?>
-            <a href="<?php the_field('url_descarga'); ?>" class="tooltipped" data-position="top" data-tooltip="descargar">
-               <b>Descargar&nbsp;<i class="fas fa-download"></i></b>
-            </a>
-        <?php } ?>
-        <?php if (get_field('url_demo')) { ?>
-            <a href="<?php the_field('url_demo'); ?>" class="tooltipped" data-position="bottom" data-tooltip="demo">
-                <b>Demostración &nbsp;<i class="fas fa-external-link-alt"></i></b>
-            </a>
-        <?php } ?>
-        <?php if (get_field('url_video')) { ?>
-            <a href="<?php the_field('url_video'); ?>" class="tooltipped" data-position="bottom" data-tooltip="demo">
-                <b>Ver en Youtube &nbsp;<i class="fab fa-youtube"></i></b>
-            </a>
-        <?php } ?>
-    </div>
-
 </div>
+
+ <!-- CATEGORIES -->
+ <nav>
+    <div class="nav-wrapper z-depth-2 mb1 blue-grey darken-3">
+        <div class="col s12"> 
+            <?php
+                $post_categories = wp_get_post_categories( get_the_ID() );
+                foreach($post_categories as $categoryId){
+                    $category = get_category( $categoryId );
+            ?>
+            <a href="/<?php echo $category->slug; ?>" class="breadcrumb roboto"><b><?php echo $category->name; ?></b></a>
+            <?php } ?>
+        </div>
+    </div>
+</nav>
+
+<?php endwhile; ?>
+
+    </div>
+</div>
+
 <?php
     get_footer();
 ?> 
