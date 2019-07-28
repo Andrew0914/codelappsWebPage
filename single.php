ffe690c03af7b057767daa6acbca5c9f1264b6a3
AndrewAlan <andrewalangm@gmail.com>
2019-07-28 00:47:53 -0500

    Show basic single post info

--- single.php
+++ single.php
@@ -10,6 +10,23 @@
             <?php the_content(); ?>
         <?php endwhile; ?>
     </div>
+    <div class="card-action">
+        <?php if (get_field('enlace')) { ?>
+            <a href="<?php the_field('enlace'); ?>" class="tooltipped <?php echo isset($options['enlace_class']) ? $options['enlace_class'] : ''; ?>" data-position="right" data-tooltip="demo">
+                <b>Ver en vivo &nbsp;<i class="fas fa-external-link-alt"></i></b>
+            </a>
+        <?php } ?>
+        <?php	if (get_field('url_descarga')) { ?>
+            <a href="<?php the_field('url_descarga'); ?>" class="tooltipped <?php echo isset($options['download_class']) ? $options['download_class'] : ''; ?>" data-position="bottom" data-tooltip="descargar">
+               <b>Descargar&nbsp;<i class="fas fa-download"></i></b>
+            </a>
+        <?php } ?>
+        <?php if (get_field('url_demo')) { ?>
+            <a href="<?php the_field('url_demo'); ?>" class="tooltipped <?php echo isset($options['demo_class']) ? $options['demo_class'] : ''; ?>" data-position="right" data-tooltip="demo">
+            <b>Demostración &nbsp;<i class="fas fa-external-link-alt"></i></b>
+            </a>
+        <?php } ?>
+    </div>
 </div>
 <?php
     get_footer();
