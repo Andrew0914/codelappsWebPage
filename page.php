ec641ef1033b66e60f819958801ebf256578143a
AndrewAlan <andrewalangm@gmail.com>
2019-07-27 22:10:41 -0500

    Show port folio in its page :+1:

--- page.php
+++ page.php
@@ -1,19 +1,15 @@
 <?php
-    include_once TEMPLATEPATH . '/content/part-postsDeveloper.php'; 
-    include_once TEMPLATEPATH . '/content/part-postsBlog.php'; 
-    include_once TEMPLATEPATH . '/content/part-postsTrabajo.php'; 
     get_header();
 ?>        
 <div class="card mt1 z-depth-2 animated fadeIn">
+    
     <div class="card-content">
-        <div class="container">
         <?php while(have_posts()): the_post(); ?>
-            <h1 class="noto grey-text text-darken-1 mt0">
-                <?php the_title(); ?>
-            </h1>
+            <div class="card-title noto grey-text text-darken-1">
+                <h2><?php the_title(); ?></h2>
+            </div>
             <?php the_content(); ?>
         <?php endwhile; ?>
-        </div>
     </div>
 </div>
 <?php
