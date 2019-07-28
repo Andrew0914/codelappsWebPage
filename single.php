eb94eddcd291e729caa4335c7b2a9319b174b593
AndrewAlan <andrewalangm@gmail.com>
2019-07-27 22:52:12 -0500

    Simple show single post :postbox:

--- single.php
+++ single.php
@@ -1,12 +1,12 @@
 <?php
-    include_once TEMPLATEPATH . '/content/part-postsDeveloper.php'; 
-    include_once TEMPLATEPATH . '/content/part-postsBlog.php'; 
-    include_once TEMPLATEPATH . '/content/part-postsTrabajo.php'; 
     get_header();
 ?>
 <div class="card mt1 z-depth-2">
     <div class="card-content">
         <?php while(have_posts()): the_post(); ?>  
+            <span class="card-title">
+                <b><?php the_title(); ?></b>
+            </span>
             <?php the_content(); ?>
         <?php endwhile; ?>
     </div>
