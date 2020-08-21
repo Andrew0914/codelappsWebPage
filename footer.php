ec67c1e11d46edf0d50fa298f957bd4daf152535
Andrew0914 <andrewalangm@gmail.com>
2020-08-20 23:38:57 -0500

    ☕ Change palette color

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
     include_once TEMPLATEPATH . '/content/part-postsTrabajo.php'; 
 ?>
 <!-- FOOTER PRINCIPAL DEL SITIO -->
-        <footer class="page-footer teal z-depth-1 round1">
+        <footer class="page-footer blue-grey darken-4 z-depth-1 round1">
             <div class="row">
                 <!-- PREVIEW ACERCA DE -->
                 <div class="col l4 m12 s12">
-                <h4 class="white-text text-darken-4">
+                <h4 class="amber-text text-accent-3">
                     Acerca de 
                 </h4>
                     <?php
@@ -21,7 +21,7 @@
                 </div>
                 <!-- PREVIEW DEVELOPER -->
                 <div class="col l4 m12 s12">
-                    <h4 class="white-texttext-darken-4 noto">
+                    <h4 class="amber-text text-accent-3 noto">
                         Lo último developer
                     </h4>
                     <?php
@@ -40,7 +40,7 @@
                 </div>
                 <!-- PREVIRE PORTAFOLIO -->
                 <div class="col l4 m12 s12">
-                    <h4 class="white-text text-darken-4 noto">
+                    <h4 class="amber-text text-accent-3 noto">
                             Lo último portafolio
                     </h4>
                     <?php
--- header.php
+++ header.php
@@ -1,69 +1,71 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>CODELAPPS</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <?php
-            wp_head();
-        ?>
-    </head>
-    <body class="grey lighten-4">
-        <!-- MENU PRINCIPAL -->
-        <header class='header-principal navbar-fixed'>
-            <nav class="blue-grey darken-4">
 
-                <div class="nav-wrapper contenedor">
-                    <a href="<?php echo home_url(); ?>" class="mbrand center-align">
-                        Codelapps
-                    </a>
-                    <a href="#" data-target="mobile-menu" class="sidenav-trigger"><i class="fas fa-bars"></i></a>
-                    <a class="right mobile-search" href="#" onclick="buscar(event)"><i class="material-icons">search</i></a>
+<head>
+    <title>CODELAPPS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php
+    wp_head();
+    ?>
+</head>
 
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="#"><i class="material-icons" onclick="buscar(event)">search</i></a></li>
-                    </ul>
+<body class="grey lighten-4">
+    <!-- MENU PRINCIPAL -->
+    <header class='header-principal navbar-fixed'>
+        <nav class="indigo darken-2">
 
-                    <?php
-                        // argumentos para desplegar el menu
-                        $args= array(
-                            "theme_location"=> "header-menu",
-                            "container_class"=>"right hide-on-med-and-down"
-                        );
+            <div class="nav-wrapper contenedor">
+                <a href="<?php echo home_url(); ?>" class="mbrand center-align amber-text text-accent-4">
+                    <img src="<?php echo get_bloginfo('template_url') ?>/images/coffe.png" width="32" height="32" />Codelapps
+                </a>
+                <a href="#" data-target="mobile-menu" class="sidenav-trigger"><i class="fas fa-bars"></i></a>
+                <a class="right mobile-search" href="#" onclick="buscar(event)"><i class="material-icons">search</i></a>
 
-                        // imprime el menu
-                        wp_nav_menu($args);
-                    ?>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#"><i class="material-icons" onclick="buscar(event)">search</i></a></li>
+                </ul>
 
-                </div>
-            </nav>
-            
-        </header>
-        <!-- SEARCH -->
-        <div class="contenedor mt1" id="buscador">
-            <form role="search" method="get" id="searchform"  action="<?php echo home_url(); ?>">
-                <div class="input-field">
-                    <input id="s" type="search" name="s" class="input-search white z-depth-5" placeholder="Buscar..." required >
-                    <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-                    <i class="material-icons search-close" onclick="closeThis('#buscador')">close</i>
-                </div>
-            </form>
-        </div>
-        
-        
-        <!-- MENU MOVIL -->
-        <?php
-            // argumentos para desplegar el menu
-            $args= array(
-                "theme_location"=> "header-menu",
-                "container_class"=>"blue-grey darken-4 sidenav mt3",
-                "container_id" => "mobile-menu"
-            );
+                <?php
+                // argumentos para desplegar el menu
+                $args = array(
+                    "theme_location" => "header-menu",
+                    "container_class" => "right hide-on-med-and-down"
+                );
 
-            // imprime el menu
-            wp_nav_menu($args);
-        ?>
-        
-        <!-- SECCION PARA EL CONTENIDO PRINCIPAL -->
-        <section class="main contenedor">  
+                // imprime el menu
+                wp_nav_menu($args);
+                ?>
+
+            </div>
+        </nav>
+
+    </header>
+    <!-- SEARCH -->
+    <div class="contenedor mt1" id="buscador">
+        <form role="search" method="get" id="searchform" action="<?php echo home_url(); ?>">
+            <div class="input-field">
+                <input id="s" type="search" name="s" class="input-search white z-depth-5" placeholder="Buscar..." required>
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons search-close" onclick="closeThis('#buscador')">close</i>
+            </div>
+        </form>
+    </div>
+
+
+    <!-- MENU MOVIL -->
+    <?php
+    // argumentos para desplegar el menu
+    $args = array(
+        "theme_location" => "header-menu",
+        "container_class" => "blue-grey darken-4 sidenav mt3",
+        "container_id" => "mobile-menu"
+    );
+
+    // imprime el menu
+    wp_nav_menu($args);
+    ?>
+
+    <!-- SECCION PARA EL CONTENIDO PRINCIPAL -->
+    <section class="main contenedor">
